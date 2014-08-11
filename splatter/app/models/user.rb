adbb23c2fe09b9e2ae23bd0db8341cc0571a5660
ashokt1 <tanvi.ashok@gmail.com>
2014-08-11 02:10:57 +0000

    Lab 4.1 Validating Data in Models

--- splatter/app/models/user.rb
+++ splatter/app/models/user.rb
@@ -1,5 +1,18 @@
 class User < ActiveRecord::Base
+
+#Performing validations
+validates :name, presence: true
+validates :email, uniqueness: { case_sensitive: false}
+validates :password, length: {minimum: 8}, if: :strong?
+
+def strong?
+	password =~ /.*\d+.*/ && \
+	password =~ /.*[a-z]+.*/ && \
+	password =~ /.*[A-Z].*/
+end
+
 has_many :splatts
+
 has_and_belongs_to_many :follows,
 	class_name: "User",
 	join_table: :follows,
