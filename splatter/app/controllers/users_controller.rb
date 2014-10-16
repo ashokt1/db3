b51546034d3430fe77d0e94b48db3c646df7b1b9
ashokt1 <tanvi.ashok@gmail.com>
2014-10-16 02:33:50 +0000

    Modified Users Controller

--- splatter/app/controllers/users_controller.rb
+++ splatter/app/controllers/users_controller.rb
@@ -127,12 +127,12 @@ before_filter :set_headers
 	end
 	
 	# GET /users/splatts-feed/1
-	 def splatts_feed
-		@feed = Splatt.find_by_sql("SELECT * FROM splatts JOIN follows ON follows.followed_id = splatts.user_id
-								   WHERE follows.follower_id = #{params[:id]} ORDER BY created_at DESC")
+	 # def splatts_feed
+		# @feed = Splatt.find_by_sql("SELECT * FROM splatts JOIN follows ON follows.followed_id = splatts.user_id
+								   # WHERE follows.follower_id = #{params[:id]} ORDER BY created_at DESC")
 		
-		 render json: @feed
-	 end
+		 # render json: @feed
+	 # end
 	
 	
 	def splatts_count
@@ -156,6 +156,39 @@ before_filter :set_headers
 		
 		User.map_reduce(map,reduce).out(inline: true)
 	end
+	
+	def splatts_feed
+		map = %Q{ function() {
+			if(this.splatts) {
+				emit("feed", {"list": this.splatts})
+			}
+		}
+		}
+				
+		reduce = %Q{ function(key, val) {
+			var myfeed = {"list": []};
+			val.forEach(function(v) {
+				myfeed.list = myfeed.list.concat(v.list);
+			});
+			return myfeed;
+		}
+		}
+		
+		finalise = %Q{ function(key, val) {
+			var mylist = val.list;
+			if(mylist) {
+				mylist.sort(function(a, b) {
+			return b.created_at - a.created_at});
+			 }
+			 return {"list": mylist};
+		}
+		}
+		
+		@user = User.find(params[:id])
+		@result = User.in(id: @user.follow_ids).map_reduce(map, reduce).out(inline: true).finalize(finalise)
+		render json: @result.entries[0][:value][:list]
+	end
+
 	
   private
 	def user_params(params)
